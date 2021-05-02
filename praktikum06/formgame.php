ae466095456a459aa307dc10ea7f23a1a392e323
Efa Setiyani <71278686+Efa72@users.noreply.github.com>
2021-05-02 21:59:58 +0700

    Add files via upload

---
+++ praktikum06/formgame.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh</title>
+</head>
+<body>
+	<h1>Test Login Game Tebak Angka</h1>
+	<?php
+	// bagian yang dieksekusi ketika pengunjung submit nama
+	if (isset($_POST['submit'])){
+	// mengeset cookie username dari namanya, lama cookie 3 bulan
+	setcookie("username", $_POST['username'], time()+3*30*24*3600,"/");
+	// mengeset cookie jumlah kunjungan -> 0 (mula-mula)
+	setcookie("visits", 1, time()+3*30*24*3600,"/");
+	// mengeset cookie kunjungan terakhir
+	setcookie("lastvisit", date("d-m-Y H:i:s"), time()+3*30*24*3600,"/");
+	// setelah mengeset cookie awal di atas, redirect ke halaman depan
+	header("Location:formprojectlatihan.php");
+}
+// jika sudah ada cookie username
+if (isset($_COOKIE['username'])){
+	// tampilkan nama user, baca dari cookie
+	echo "<p>Hallo selamat datang ".$_COOKIE['username']."</p>";
+	// tampilkan jumlah kunjungan saat ini = jumlah visit sebelumnya + 1
+	echo "<p>Ini kunjungan Anda yang ke-".($_COOKIE['visits']++)."</p>";
+	// tampilkan datetime kunjungan sebelumnya, baca dari cookie
+	echo "<p>Kunjungan Anda sebelumnya adalah pada tanggal ".$_COOKIE['lastvisit']."</p>";
+	// setelah cookie sebelumnya dibaca, lakukan update cookie yang baru
+	setcookie("username", $_COOKIE['username'], time()+3*30*24*3600,"/");
+	setcookie("visits", $_COOKIE['visits'], time()+3*30*24*3600,"/");
+	setcookie("lastvisit", date("d-m-Y H:i:s"), time()+3*30*24*3600,"/");
+	echo "<p>Ayo main <a href='game.php'>game</a> ...</p>";
+}
+ else {
+// jika cookie username belum ada, munculkan form
+?>
+	<h1>Welcome to my site</h1>
+	<p>Ini kunjungan Anda pertama kali di situs ini ya?</p>
+	<p>Kita kenalan dulu ya, silakan masukkan nama Anda</p>
+	<form method="post" action="formprojectlatihan.php">
+		Nama Anda <input type="text" name="username">
+		<input type="submit" name="submit" value="Submit">
+	</form>	
+}
+?>
+</body>
+</html>
---
+++ praktikum06/game.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh</title>
+</head>
+<body>
+	<?php
+	
+	?>
+	<h2>GAME TEBAK ANGKA</h2>
+	<p> Hallo, nama saya Mr. PHP. Saya telah memilih secara random sebuah bilangan bulat. Silakan Anda menebak ya!</p>
+	<p>Masukkan Bilangan Tebakan Anda</p>
+	<form method="post" action="game.php">
+		<input type="text" name="bil">
+
+		<input type="submit" name="submit" value="tebak">
+	</form>
+
+	<?php
+	
+	if (isset($_POST['bil'])){
+	$bil = $_POST['bil'];
+	$acak = rand(0,100);
+	
+
+	if ($bil==$acak) {
+		echo "<h4>Selamat ya… ".$_COOKIE['username']." Anda benar, saya telah memilih bilangan ".$acak;
+		echo "<h3>Ayo <a href='game.php'>main</a> lagi...";
+	}
+	if ($bil<$acak) {
+		echo "<h5>Waaah… masih salah ya, bilangan tebakan Anda terlalu rendah.";
+	}
+	if ($bil>$acak) {
+		echo "<h5>Waaah… masih salah ya, bilangan tebakan Anda terlalu tinggi.";
+	}
+}
+?>	
+	
+</body>
+</html>
+
